581144236463fa6314f2467f8701af2ca717cf66
Sakib Ahmed <alisakib543@gmail.com>
2025-04-29 22:15:16 +0500

    initial changes

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express();
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    retryWrites: true
 })
     .then(() => {
         console.log('Connected to MongoDB');
