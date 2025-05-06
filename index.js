56353b3ef648028e6f9cd5b02dea9dbb6a753e03
Sakib Ahmed <alisakib543@gmail.com>
2025-05-06 23:47:02 +0500

    Port changed

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const app = express();
 
 //mongodb connection
 mongoose.connect(process.env.MONGO_URI, {
- useNewUrlParser: true,
-  useUnifiedTopology: true,
+//  useNewUrlParser: true,
+//   useUnifiedTopology: true,
   serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 10s
   retryWrites: true,
 })
@@ -32,6 +32,6 @@ app.use('/api/booking-logs', require('./Routes/BookingLogroutes'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT , () => {
-    console.log('Server is running on port',PORT);
+app.listen(PORT, () => {
+    console.log(`Server is running on port${PORT}`);
 })
